f67ec31d31eb18def21946be4d512fa64f276c1d
foxleren <64990498+foxleren@users.noreply.github.com>
2020-12-03 11:30:17 +0300

    Styles for input-file

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -73,3 +73,33 @@ function hasScrolled() {
 
 	lastScrollTop = st;
 }
+
+
+
+
+
+
+
+
+
+
+
+/*----------------------- */
+
+
+let fields = document.querySelectorAll('.upload-file-input');
+    Array.prototype.forEach.call(fields, function (input) {
+      let label = input.nextElementSibling,
+        labelVal = label.querySelector('.upload-file-status').innerText;
+ 
+      input.addEventListener('change', function (e) {
+        let countFiles = '';
+        if (this.files && this.files.length >= 1)
+          countFiles = this.files.length;
+ 
+        if (countFiles)
+          label.querySelector('.upload-file-status').innerText = 'Выбрано файлов: ' + countFiles;
+        else
+          label.querySelector('.upload-file-status').innerText = labelVal;
+      });
+    });
